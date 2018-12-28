5e637959f272c1486bf40cd350283a972704621f
unknown <chris.a.desilva@gmail.com>
2018-12-27 21:03:43 -0600

    completed addition functionality

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,28 @@
+//generate random numbers
 function getRandomNumber(){
     const minNum = 0;
-    const maxNum = 12;
-    return Math.floor(Math.random() * (maxNum +1) + minNum)
+    const maxNum = 100;
+    return Math.floor(Math.random() * (maxNum +1) + minNum);
+    
 }
-
+//assign random numbers to top and bottom
 function getProblem(){
     document.getElementById("problemTop").innerHTML = getRandomNumber();
     document.getElementById("bottomNumber").innerHTML = getRandomNumber();
+    document.getElementById("answerBox").value = "";
 }
+//read top, bottom, and answer numbers to tell user if answer is correct
+function checkAnswer(){
+    let topNumber = document.getElementById("problemTop").innerHTML;
+    let bottomNumber = document.getElementById("bottomNumber").innerHTML;
+    let answer = document.getElementById("answerBox").value;
 
-
+    if(Number(topNumber) + Number(bottomNumber) == Number(answer)){
+        alert("Nice!");
+        return getProblem();
+    } else {
+        alert("Try again!");
+    }
+}
 
 window.onload = getProblem;
